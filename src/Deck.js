5d1330e32217cae79887da52bebfa05b4e0c7c1c
Andrew Mogg <andrewmogg@Andrews-MacBook-Pro.local>
2017-07-17 09:26:30 -0400

    top card can now rotate in and out

--- src/Deck.js
+++ src/Deck.js
@@ -3,32 +3,64 @@ import {
   View,
   Style,
   Text,
-  PanResponder
+  PanResponder,
+  Animated
 } from 'react-native';
 class Deck extends Component {
 
  constructor(props){
    super(props);
 
+   const position = new Animated.ValueXY();
    const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
 			onPanResponderMove: (event, gesture) => {
-          console.log(gesture)
+        position.setValue({ x: gesture.dx, y: gesture.dy }); //update current position with gesture motion
       },
 			onPanResponderRelease: () => {}
    });
-   this.state = { panResponder };
+   this.state = { panResponder, position };
 
  }
 
+  getCardStyle(){
+    //destructuring for access within scope
+    const { position } = this.state;
+
+    //interpolation allows us to relate x movement with y scale movement
+    const rotate = position.x.interpolate({
+      inputRange: [-500, 0 , 500],
+      outputRange: ['-120deg', '0deg' , '120deg']
+
+    });
+
+    return {//pass the current position to the style
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      //only animate the first card on the screen
+      if (index === 0)
+      {
+          return (
+            <Animated.View
+              key={item.id}
+              style={this.getCardStyle()}
+              {...this.state.panResponder.panHandlers}
+            >
+              {this.props.renderCard(item)}
+            </Animated.View>
+          );
+      }
       return this.props.renderCard(item);
     });
   }
   render() {
     return (
-      <View {...this.state.panResponder.panHandlers}>
+      <View>
         {this.renderCards()}
       </View>
     );
